86cd755de33de0c8cea8c6e4f16bf9971ca17d97
Vishal Changder <personal.vishal.1999changder@gmail.com>
2026-03-19 19:53:25 +0530

    Project setup with folder management

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="border">Hello</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<div className="">Home</div>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
